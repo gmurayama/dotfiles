14b5922e2d84c729e0aa9f98a4b39d4e029bd6d2
Gustavo Murayama <gmurayama@outlook.com>
2023-12-28 18:22:51 -0300

    live_grep hidden files on nvim config

--- nvim/lua/plugins/nvim.lua
+++ nvim/lua/plugins/nvim.lua
@@ -6,5 +6,25 @@ if true then
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = {
+        filesystem = {
+          filtered_items = { hide_dotfiles = false, hide_hidden = false },
+        },
+      },
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      opts = {
+        pickers = {
+          live_grep = {
+            additional_args = function(_)
+              return { "--hidden" }
+            end,
+          },
+        },
+      },
+    },
   }
 end
