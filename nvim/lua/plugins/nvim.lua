4d6479d7636c0edbe988af58134e53180c313685
Gustavo Murayama <gmurayama@outlook.com>
2023-12-28 17:29:56 -0300

    enable nvim extra features for languages

--- nvim/lua/plugins/nvim.lua
+++ nvim/lua/plugins/nvim.lua
@@ -1,5 +1,10 @@
 if true then
   return {
-    {},
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.dap.core" },
   }
 end
