c4c5ff1768e4e3e663c5459272b277492e2f547b
Gustavo Murayama <gmurayama@outlook.com>
2024-01-08 14:40:21 -0300

    filter folders on neo-tree

--- nvim/lua/plugins/nvim.lua
+++ nvim/lua/plugins/nvim.lua
@@ -10,7 +10,11 @@ if true then
       "nvim-neo-tree/neo-tree.nvim",
       opts = {
         filesystem = {
-          filtered_items = { hide_dotfiles = false, hide_hidden = false },
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_hidden = false,
+            hide_by_name = { ".git", "node_modules" },
+          },
         },
       },
     },
