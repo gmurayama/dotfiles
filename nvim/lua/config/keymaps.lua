2d935f226f41183b1f260b1bdb78f56253ede59f
Gustavo Murayama <gmurayama@outlook.com>
2024-03-13 20:29:34 -0300

    feat: change shortcuts

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,4 +12,28 @@ if vim.g.vscode then
 else
   vim.keymap.set("n", "]<tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next Tab" })
   vim.keymap.set("n", "[<tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Prev Tab" })
+
+  -- Switch commands to move between windows and resize window
+  vim.keymap.del("n", "<C-h>")
+  vim.keymap.del("n", "<C-j>")
+  vim.keymap.del("n", "<C-k>")
+  vim.keymap.del("n", "<C-l>")
+  vim.keymap.del("n", "<C-Left>")
+  vim.keymap.del("n", "<C-Down>")
+  vim.keymap.del("n", "<C-Up>")
+  vim.keymap.del("n", "<C-Right>")
+
+  vim.keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
+  vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
+  vim.keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
+  vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+  vim.keymap.set("n", "<C-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+  vim.keymap.set("n", "<C-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+  vim.keymap.set("n", "<C-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+  vim.keymap.set("n", "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+  -- Debugger
+  vim.keymap.set("n", "<F11>", "<leader>di", { desc = "Step into", remap = true })
+  vim.keymap.set("n", "<F10>", "<leader>dO", { desc = "Step over", remap = true })
 end
