6d9b7669641d18f2ce6660e4fad90dd27554c321
Gustavo Murayama <gmurayama@outlook.com>
2026-06-23 15:43:34 -0300

    feat(nvim): disable auto_preview in trouble

--- nvim/lua/plugins/nvim.lua
+++ nvim/lua/plugins/nvim.lua
@@ -66,6 +66,12 @@ return {
     "itchyny/vim-qfedit",
   },
   {
+    "folke/trouble.nvim",
+    opts = {
+      auto_preview = false,
+    },
+  },
+  {
     "ibhagwan/fzf-lua",
     opts = {
       grep = {
@@ -74,4 +80,13 @@ return {
       },
     },
   },
+  --{
+  --  "iamcco/markdown-preview.nvim",
+  --  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --  build = "npm install",
+  --  init = function()
+  --    vim.g.mkdp_filetypes = { "markdown" }
+  --  end,
+  --  ft = { "markdown" },
+  --},
 }
